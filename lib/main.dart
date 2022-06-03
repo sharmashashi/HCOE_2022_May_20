1d7230d3f0757e7202e2b5daa59dd8e19458a273
Shashi Sharma <sharmashashi940@gmail.com>
2022-06-03 08:03:53 +0545

    sample http get

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:firstapp/feeds/feedsV.dart';
+import 'package:firstapp/fetchfrominternet/sample_fetch_pageV.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
   runApp(GetMaterialApp(
-    home: Feeds(),
+    home: SampleFetchPage(),
   ));
 }
